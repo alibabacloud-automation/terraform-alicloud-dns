59a3c735b533f77b97f1460e033ad3dff7f5d1f0
liuyuan <709503583@qq.com>
2021-11-22 14:28:44 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/dns"
-}
-
 data "alicloud_dns_groups" "this" {
   name_regex = var.existing_group_name
 }
